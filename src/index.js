0d9191e4eaf474242466cec86024d118519ddddd
spur237 <samuel.nzioka@student.moringaschool.com>
2022-08-31 12:42:57 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 let addToy = false;
-
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
